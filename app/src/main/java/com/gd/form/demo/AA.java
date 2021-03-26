d1f30984e57bec9ea783acb51c8fdea881847732
xue jinzhu <xuejz@ailng.com>
2021-03-26 17:05:19 +0800

    解决冲突测试

--- app/src/main/java/com/gd/form/demo/AA.java
+++ app/src/main/java/com/gd/form/demo/AA.java
@@ -1,4 +1,30 @@
 package com.gd.form.demo;
 
 public class AA {
+    private String address;
+    private int age;
+    private String name;
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
