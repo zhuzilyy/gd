3369e7328d28d964e795b757fc786c8afa0f9778
lijx <ljxingumai@163.com>
2021-03-26 17:06:33 +0800

    aa

--- app/src/main/java/com/gd/form/demo/AA.java
+++ app/src/main/java/com/gd/form/demo/AA.java
@@ -1,4 +1,14 @@
 package com.gd.form.demo;
 
 public class AA {
+
+    private String aa;
+
+    public String getAa() {
+        return aa;
+    }
+
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
 }
