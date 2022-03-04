4fb7ae377c385a628113b84cde97424991327f08
xue jinzhu <xuejz@ailng.com>
2022-03-04 18:14:58 +0800

    去掉阿里云oos的常量

--- app/src/main/java/com/gd/form/constants/Constant.java
+++ app/src/main/java/com/gd/form/constants/Constant.java
@@ -9,10 +9,7 @@ public class Constant {
     public static final int SUCCESS_CODE = 200;
     public static final int PIPE_DEPARTMENT = 400;
     public static final int CORRODE_DEPARTMENT = 100;
-    public static String ENDPOINT = "https://oss-cn-beijing.aliyuncs.com";
-    public static String ACCESSKEYID = "LTAI5t6GvrwQ4TnLLSvP78DA";
-    public static String ACCESSKEYSECRET = "ptx8dILUl3YHyaMXLOJUgkgUFePJTw";
-    public static String BUCKETSTRING = "tianran-beijing";
+
 //    public static String ENDPOINT;
 //    public static String ACCESSKEYID;
 //    public static String ACCESSKEYSECRET;
