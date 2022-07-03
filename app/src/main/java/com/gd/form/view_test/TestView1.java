2fd1682a34345d52c761f1bad56057539290fcfe
xue jinzhu <xuejz@ailng.com>
2022-07-03 18:24:43 +0800

    添加测试View11

---
+++ app/src/main/java/com/gd/form/view_test/TestView1.java
@@ -0,0 +1,15 @@
+package com.gd.form.view_test;
+
+import com.gd.form.base.BaseActivity;
+
+public class TestView1 extends BaseActivity {
+    @Override
+    protected void setStatusBar() {
+
+    }
+
+    @Override
+    protected int getActLayoutId() {
+        return 0;
+    }
+}
