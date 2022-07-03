c8a89814bcd64845700649af167587a136c0214b
xue jinzhu <xuejz@ailng.com>
2022-07-03 16:27:22 +0800

    添加测试activity
    # Conflicts:
    #	app/src/main/java/com/gd/form/activity/TestActivity.java

---
+++ app/src/main/java/com/gd/form/activity/TestActivity.java
@@ -0,0 +1,16 @@
+package com.gd.form.activity;
+
+import com.gd.form.base.BaseActivity;
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected void setStatusBar() {
+
+    }
+
+    @Override
+    protected int getActLayoutId() {
+        return 0;
+    }
+}
