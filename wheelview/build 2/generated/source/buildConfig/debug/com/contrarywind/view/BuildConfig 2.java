8f740bed23752fb60c2fd0a9f600db91218e97a9
xue jinzhu <xuejz@ailng.com>
2022-07-03 20:05:14 +0800

    删除多余文件

---
+++ wheelview/build 2/generated/source/buildConfig/debug/com/contrarywind/view/BuildConfig 2.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.contrarywind.view;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.contrarywind.view";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
+  public static final String APPLICATION_ID = "com.contrarywind.view";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 31;
+  public static final String VERSION_NAME = "4.1.0";
+}
