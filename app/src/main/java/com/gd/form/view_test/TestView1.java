a259ae15db46387bf464de2ad2d9f1dbe91dc741
xue jinzhu <xuejz@ailng.com>
2022-07-03 18:25:14 +0800

    添加测试View11

--- app/src/main/java/com/gd/form/view_test/TestView1.java
+++ app/src/main/java/com/gd/form/view_test/TestView1.java
@@ -1,11 +1,13 @@
 package com.gd.form.view_test;
 
+import android.util.Log;
+
 import com.gd.form.base.BaseActivity;
 
 public class TestView1 extends BaseActivity {
     @Override
     protected void setStatusBar() {
-
+        Log.i("tag","测试");
     }
 
     @Override
