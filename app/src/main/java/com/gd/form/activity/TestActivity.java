cbb9f1832df886a2166627b6f17530db1544b88e
xue jinzhu <xuejz@ailng.com>
2022-07-03 16:27:22 +0800

    添加测试activity

--- app/src/main/java/com/gd/form/activity/TestActivity.java
+++ app/src/main/java/com/gd/form/activity/TestActivity.java
@@ -1,4 +1,16 @@
 package com.gd.form.activity;
 
-public class TestActivity {
+import com.gd.form.base.BaseActivity;
+
+public class TestActivity extends BaseActivity {
+
+    @Override
+    protected void setStatusBar() {
+
+    }
+
+    @Override
+    protected int getActLayoutId() {
+        return 0;
+    }
 }
