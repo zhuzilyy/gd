a593ea3d7c5dda66930f7bc6acb01b6175dc6d83
xue jinzhu <xuejz@ailng.com>
2022-07-03 18:24:12 +0800

    添加测试View11

--- app/src/main/java/com/gd/form/activity/TestActivity.java
+++ app/src/main/java/com/gd/form/activity/TestActivity.java
@@ -1,12 +1,14 @@
 package com.gd.form.activity;
 
+import android.util.Log;
+
 import com.gd.form.base.BaseActivity;
 
 public class TestActivity extends BaseActivity {
 
     @Override
     protected void setStatusBar() {
-
+        Log.i("tag","2222222222");
     }
 
     @Override
--- app/src/main/java/com/gd/form/view_test/TestView.java
+++ app/src/main/java/com/gd/form/view_test/TestView.java
@@ -1,4 +1,9 @@
 package com.gd.form.view_test;
 
+import android.util.Log;
+
 public class TestView {
+    public static void main(String[] args) {
+        Log.i("tag","33333333333333");
+    }
 }
